835790919f1bd39172ea1f45dc59a4674bb85660
Swaranjana Nayak <58822437+sn2606@users.noreply.github.com>
2021-04-25 15:57:43 +0530

    Profile page updated

--- HTML/profile.php
+++ HTML/profile.php
@@ -1,8 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>FaceClone</title>
-
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>TravelLog</title>
+  <link rel="icon" type="image/png" href="../Images/alps_favicon.png">
+  <meta name="description" content="Connect with people over travelling">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -12,19 +16,9 @@
 </head>
 <body>
   <!-- nav -->
-  <nav class="navbar navbar-default">
-    <div class="container-fluid">
-      <div class="navbar-header">
-        <a class="navbar-brand" href="index.html">FaceClone</a>
-      </div>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="home.html">Home</a></li>
-        <li><a href="profile.html">Profile</a></li>
-        <li><a href="#">Logout</a></li>
-      </ul>
-    </div>
-  </nav>
-  <!-- ./nav -->
+  <?php
+  include "header.php";
+  ?>
 
   <!-- main -->
   <main class="container">
@@ -55,11 +49,11 @@
         <!-- user profile -->
         <div class="media">
           <div class="media-left">
-            <img src="img/my_avatar.png" class="media-object" style="width: 128px; height: 128px;">
+            <img src="../Images/Background/pexels-dominika-roseclay-1252500.jpg" class="media-object" style="width: 128px; height: 128px;">
           </div>
           <div class="media-body">
-            <h2 class="media-heading">nicholaskajoh</h2>
-            <p>Status: I love to code!, Location: Nigeria</p>
+            <h2 class="media-heading">swaranajna</h2>
+            <p>Status: I love to code!, Location: India</p>
           </div>
         </div>
         <!-- user profile -->
@@ -71,10 +65,10 @@
           <!-- post -->
           <div class="panel panel-default">
             <div class="panel-body">
-              <p>Hello people! This is my first FaceClone post. Hurray!!!</p>
+              <p>Hello people! This is my first TravelLog post. Hurray!!!</p>
             </div>
             <div class="panel-footer">
-              <span>posted 2017-5-27 20:45:01 by nicholaskajoh</span> 
+              <span>posted 2021-4-21 20:45:01 by swaranjana</span> 
               <span class="pull-right"><a class="text-danger" href="#">[delete]</a></span>
             </div>
           </div>
@@ -102,11 +96,9 @@
   <!-- ./main -->
 
   <!-- footer -->
-  <footer class="container text-center">
-    <ul class="nav nav-pills pull-right">
-      <li>FaceClone - Made by [your name here]</li>
-    </ul>
-  </footer>
+  <?php
+  include "footer.php";
+  ?>
   <!-- ./footer -->
   <script type="text/javascript" src="js/bootstrap.min.js"></script>
   <script type="text/javascript" src="js/script.js"></script>
