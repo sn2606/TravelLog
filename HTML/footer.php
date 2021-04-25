5cac3c1962ca987c8cc72942e8359f69aef609b5
Swaranjana Nayak <58822437+sn2606@users.noreply.github.com>
2021-04-25 15:25:29 +0530

    Header and footer addded to the page

---
+++ HTML/footer.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>TravelLog</title>
+  <link rel="icon" type="image/png" href="../Images/alps_favicon.png">
+  <meta name="description" content="Connect with people over travelling">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../CSS/footer.css">
+  <!-- google font api -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@300&family=Montserrat&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+</head>
+
+
+<body>
+  <!-- footer section -->
+  <footer class="footer-distributed">
+    <div class="footer-top">
+      <p class="footer-links">
+        <a href="about-us.php">About Us</a>
+        <a href="contact-us.php">Contact Us</a>
+      </p>
+    </div>
+    <div class="footer-icons">
+      <a href="https://www.facebook.com/"><i class="fab fa-facebook"></i></a>
+      <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+      <a href="https://www.linkedin.com/"><i class="fab fa-linkedin"></i></a>
+      <a href="https://github.com/sn2606/TravelLog"><i class="fab fa-github"></i></a>
+    </div>
+    <hr>
+    <p class="footer-company-name"><strong>TravelLog &copy; 2021</strong></p>
+  </footer>
+</body>
+
+</html>
---
+++ HTML/header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>TravelLog</title>
+    <link rel="icon" type="image/png" href="../Images/alps_favicon.png">
+    <meta name="description" content="Connect with people over travelling">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../CSS/header.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <!-- navbar -->
+    <div class="nav-wrapper">
+        <nav class="navbar">
+            <div class="Logo">
+                <img src="../Images/mountain.png" class="Logo-Img" alt="Alps-Logo" usemap="#home">
+                <a href="../index.php"><span class="Logo-Text">TRAVELLOG</span></a>
+            </div>
+            <div class="navigation">
+                <ul class="menu">
+                    <li><a href="../index.php"><i class="fas fa-home"></i></a></li>
+                    <li><a href="#"><i class="fas fa-bell"></i></a></li>
+                    <li><a href="#"><i class="fas fa-user-alt"></i></a></li>
+                </ul>
+                <div class="not-menu">
+                    <div class="search">
+                        <div class="search-content">
+                            <button class="search-button"><i class="fas fa-search"></i></button>
+                            <input type="text" class="search-input" placeholder="Search here...">
+                        </div>
+                    </div>
+                    <div class="sign-out">
+                        <a href="../index.php"><span class="menu-text">Sign Out</span></a>
+                    </div>
+                    <div class="sign-out-mobile">
+                        <a href="../index.php"><i class="fas fa-sign-out-alt"></i></a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+
+</body>
+
+</html>
