21110050c0f06d0a3875ded46322fa41cf0e5ca0
Swaranjana Nayak <58822437+sn2606@users.noreply.github.com>
2021-04-27 22:39:33 +0530

    Session start DRY

--- PHP/functions.php
+++ PHP/functions.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
     function dbConnect() {
         global $conn;
         $dbServer = "localhost";
--- PHP/login.php
+++ PHP/login.php
@@ -1,7 +1,3 @@
-<?php
-session_start();
-?>
-
 <!DOCTYPE html>
 <html>
 
