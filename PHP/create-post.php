be1a91c906471ab55bb3e21d2aeaed35be3fc3ef
Swaranjana Nayak <58822437+sn2606@users.noreply.github.com>
2021-05-04 11:14:41 +0530

    Session variables altered, create-post.php changes

--- PHP/create-post.php
+++ PHP/create-post.php
@@ -20,10 +20,10 @@ if (isset($_POST['post'])) {
         // $sql = "INSERT INTO posts (user_id, content, content_img, created_at) VALUES (?, ?, ?, ?)";
         $content = $_POST['content'];
         $blobimg = file_get_contents($_FILES['get-photo']['tmp_name']);
-        $zero = 0;
+        $uid = $_SESSION['userid'];
         $date = date('Y/m/d H:i:s');
         $statement = $conn->prepare("INSERT INTO posts (user_id, content, content_img, created_at) VALUES (?, ?, ?, ?)");
-        $statement->bind_param('isss', $zero, $content, $blobimg, $date);
+        $statement->bind_param('isss', $uid, $content, $blobimg, $date);
         // $statement->bind_param('s', $content);
         // $statement->bind_param('b', $blobimg);
         // $statement->bind_param('s', $date);
--- PHP/login.php
+++ PHP/login.php
@@ -51,6 +51,7 @@
         $_SESSION['name'] = $userpass['name'];
         $_SESSION['username'] = $username;
         $_SESSION['post-flag'] = 1;
+        $_SESSION['userid'] = $userres['user_id'];
         ?>
             <script>
               location.replace("homepage.php");
