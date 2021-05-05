ad4445bb14be6208624dd71e3e7abd796b2d08ec
Swaranjana Nayak <58822437+sn2606@users.noreply.github.com>
2021-05-05 13:44:00 +0530

    Changed some URLs

--- JS/change-bg.js
+++ JS/change-bg.js
@@ -10,8 +10,6 @@ window.onload = function(){
         'url(../Images/Mobile-Background/pexels-taryn-elliott-3889855.jpg)',
         'url(../Images/Mobile-Background/pexels-alex-azabache-3214958.jpg)',
         'url(../Images/Mobile-Background/pexels-ketut-subiyanto-4429452.jpg)',
-        'url(../Images/Background/pexels-dominika-roseclay-1252500.jpg)',
-        'url(../Images/Mobile-Background/pexels-michael-block-3225529.jpg)',
         'url(../Images/Mobile-Background/pexels-simon-migaj-885880.jpg)',
       ]
       
@@ -27,16 +25,12 @@ window.onload = function(){
       
     //   Background Images
       var images = [
-          'url(../Images/Background/img1.jpg)', 
-          'url(../Images/Background/img2.jpg)',
+          'url(../Images/Background/img1.jpg)',
           'url(../Images/Background/img3.jpg)',
-          'url(../Images/Background/img4jpg)',
           'url(../Images/Background/img5.jpg)',
-          'url(../Images/Background/pexels-andrei-tanase-1271619.jpg)',
           'url(../Images/Background/pexels-dominika-roseclay-1252500.jpg)',
           'url(../Images/Background/pexels-ketut-subiyanto-4436363.jpg)',
           'url(../Images/Background/pexels-nappy-1058959.jpg)',
-          'url(../Images/Background/pexels-pixabay-531321.jpg)',
           'url(../Images/Background/pexels-mohamed-almari-368893.jpg)',
       ]
       
