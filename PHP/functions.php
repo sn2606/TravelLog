59c7dedc38cfe49a401f163a0eab02afe7290a42
Swaranjana Nayak <58822437+sn2606@users.noreply.github.com>
2021-05-05 10:32:42 +0530

    mysqli ping

--- PHP/functions.php
+++ PHP/functions.php
@@ -1,29 +1,39 @@
 <?php
-    session_start();
-    function dbConnect() {
-        global $conn;
-        $dbServer = "localhost:3308";
-        $username = "root";
-        $password = "";
-        $dbName = "travellog";
-        $conn = new mysqli($dbServer, $username, $password, $dbName);
+session_start();
+function dbConnect()
+{
+    global $conn;
+    $dbServer = "localhost:3308";
+    $username = "root";
+    $password = "";
+    $dbName = "travellog";
+    $conn = new mysqli($dbServer, $username, $password, $dbName);
 
-        if($conn->connect_error){
-            die("Error: ".$conn->connect_error);
-        }
-        // echo '<h1 style="color: green;">Connected to DB!</h1>';
-    }
-
-    function redirect_to($location){
-        header('Location:'.$location);
+    if ($conn->ping()) {
+        // printf ("Our connection is ok!\n");
+    } else {
+        printf("Error: %s\n", $conn->error);
     }
 
-    function is_auth() {
-        return isset($_SESSION['userid']);
+    if ($conn->connect_error) {
+        die("Error: " . $conn->connect_error);
     }
-    
-    function check_auth() {
-        if(!is_auth()) {
-            redirect_to("/index.php?logged_in=false");
-        }
+    // echo '<h1 style="color: green;">Connected to DB!</h1>';
+}
+
+function redirect_to($location)
+{
+    header('Location:' . $location);
+}
+
+function is_auth()
+{
+    return isset($_SESSION['userid']);
+}
+
+function check_auth()
+{
+    if (!is_auth()) {
+        redirect_to("/index.php?logged_in=false");
     }
+}
